1c2e08c3fe4ea9316a4056493ee5dc52bc1d299f
ErikTrapman <veggatron@gmail.com>
2013-02-24 14:38:42 +0100

    cosmetics

--- Form/WedstrijdType.php
+++ Form/WedstrijdType.php
@@ -12,7 +12,7 @@ class WedstrijdType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('datum', 'date', array('format' => 'dd-MM-y','data'=> new \DateTime()))
+            ->add('datum', 'date', array('format' => 'dd-MM-y'))
             ->add('naam')
             ->add('uitslagtype', 'entity', array('class' => 'CyclearGameBundle:UitslagType'))
             ->add('seizoen', 'seizoen_selector')
