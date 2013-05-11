0276813819ed618c9987ace968cb97f66faf6694
Erik Trapman <veggatron@gmail.com>
2013-05-11 17:54:48 +0200

    removed unused bundle

--- Controller/Admin/DefaultController.php
+++ Controller/Admin/DefaultController.php
@@ -20,15 +20,6 @@ class DefaultController extends Controller
     public function indexAction(\Symfony\Component\HttpFoundation\Request $request)
     {
 
-        $form = $this->createForm('eriktrapman_commandselectortype');
-        if ($request->getMethod() == 'POST') {
-            $form->bind($request);
-            if ($form->isValid()) {
-                $event = new \ErikTrapman\Bundle\WebCommandBundle\Event\CommandRunEvent($form->get('command')->getData(), $form->get('options')->getData());
-                $this->get('event_dispatcher')->dispatch('eriktrapman.command.run', $event);
-                return $this->redirect($this->generateUrl('admin_index'));
-            }
-        }
-        return array('form' => $form->createView());
+        return array();
     }
 }
