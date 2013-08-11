1279af484c6583960f9e7e2964f14d34943a4cf1
Erik Trapman <veggatron@gmail.com>
2013-08-11 11:20:31 +0200

    moved some stuff to application and out of the bundle

--- DependencyInjection/CyclearGameExtension.php
+++ DependencyInjection/CyclearGameExtension.php
@@ -23,7 +23,6 @@ class CyclearGameExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services_logging.xml');
         $loader->load('services.xml');
         
         $container->setParameter('enable_twitter', $config['enable_twitter']);
