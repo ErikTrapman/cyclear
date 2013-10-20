440019533d6e9c908e8ed85f390942697941ea73
Erik Trapman <veggatron@gmail.com>
2013-10-20 16:27:19 +0200

    - tweaking mobile/desktop view - fixed autocomplete on ajax-content

--- Resources/public/js/typeahead.js
+++ Resources/public/js/typeahead.js
@@ -1,26 +1,27 @@
-$(document).ready(function(){
-    
-    $('.ajax-typeahead').typeahead({
-        source: function(query, process) {
-            return $.ajax({
-                url: $(this)[0].$element[0].dataset.link,
-                type: 'get',
-                data: {
-                    query: query
-                },
-                dataType: 'json',
-                success: function(json) {
-                    var options = [];
-                    for(i in json){
-                        options.push(json[i].naam);
+$(document).ready(function () {
+
+    $(document).on("focus", ".ajax-typeahead", function () {
+        $(this).typeahead({
+            source: function (query, process) {
+                return $.ajax({
+                    url: $(this)[0].$element[0].dataset.link,
+                    type: 'get',
+                    data: {
+                        query: query
+                    },
+                    dataType: 'json',
+                    success: function (json) {
+                        var options = [];
+                        for (i in json) {
+                            options.push(json[i].naam);
+                        }
+                        typeof json == 'undefined' ? false : process(options);
+                        return;
                     }
-                    typeof json == 'undefined' ? false : process(options);
-                    return;
-                }
-            });
-        },
-        minLength: 3,
-        items: 20 /* TODO how to get this from config.yml? */
+                });
+            },
+            minLength: 3,
+            items: 20
+        });
     });
-    
 });
