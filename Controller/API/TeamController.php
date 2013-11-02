ff4c50502207c3aa10a2453873f57399028b37e5
Erik Trapman <veggatron@gmail.com>
2013-11-02 19:56:54 +0100

    proper license and copyright

--- Controller/API/TeamController.php
+++ Controller/API/TeamController.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Cyclear-game package.
+ *
+ * (c) Erik Trapman <veggatron@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Cyclear\GameBundle\Controller\API;
 
 use FOS\RestBundle\Controller\Annotations\QueryParam;
