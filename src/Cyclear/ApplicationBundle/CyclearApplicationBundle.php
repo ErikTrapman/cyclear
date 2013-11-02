59233f80fbdd4c4e4b3ebf3f3ddffab4b806d980
Erik Trapman <veggatron@gmail.com>
2013-11-02 19:52:27 +0100

    proper license

--- src/Cyclear/ApplicationBundle/CyclearApplicationBundle.php
+++ src/Cyclear/ApplicationBundle/CyclearApplicationBundle.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Cyclear-game package.
+ *
+ * (c) Erik Trapman <veggatron@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Cyclear\ApplicationBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
