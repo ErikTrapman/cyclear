48338025c554456bdf8e60df3638decf463cbb3c
Erik Trapman <veggatron@gmail.com>
2013-11-10 13:06:39 +0100

    fix start-date, enddate back to db-result

--- Controller/API/TeamController.php
+++ Controller/API/TeamController.php
@@ -27,11 +27,11 @@ class TeamController extends \FOS\RestBundle\Controller\FOSRestController
         $seizoen = $em->getRepository("CyclearGameBundle:Seizoen")->findOneBySlug($seizoen);
 
         $ploegen = $em->getRepository("CyclearGameBundle:Ploeg")
-                ->createQueryBuilder('p')
-                ->where('p.seizoen =:seizoen')
-                ->orderBy('p.afkorting')
-                ->setParameter('seizoen', $seizoen)
-                ->getQuery()->getResult();
+            ->createQueryBuilder('p')
+            ->where('p.seizoen =:seizoen')
+            ->orderBy('p.afkorting')
+            ->setParameter('seizoen', $seizoen)
+            ->getQuery()->getResult();
         $map = array();
         foreach ($em->getRepository("CyclearGameBundle:Uitslag")->getPuntenByPloeg($seizoen) as $result) {
             $map[$result['id']] = $result['punten'];
@@ -40,12 +40,12 @@ class TeamController extends \FOS\RestBundle\Controller\FOSRestController
             $ploeg->setPunten($map[$ploeg->getId()]);
         }
         // sort
-        usort($ploegen, function($a, $b) {
-                if ($a->getPunten() == $b->getPunten()) {
-                    return 0;
-                }
-                return ($a->getPunten() < $b->getPunten()) ? 1 : -1;
-            });
+        usort($ploegen, function ($a, $b) {
+            if ($a->getPunten() == $b->getPunten()) {
+                return 0;
+            }
+            return ($a->getPunten() < $b->getPunten()) ? 1 : -1;
+        });
         $view = $this->view($ploegen, 200)->setSerializationContext(SerializationContext::create()->setGroups(array('medium')));
         return $this->handleView($view);
     }
@@ -78,15 +78,26 @@ class TeamController extends \FOS\RestBundle\Controller\FOSRestController
         $map = array();
         $teampoints = array();
         foreach ($stmt->fetchAll() as $row) {
-            $teampoints[$row['ploeg_id']] = ( array_key_exists($row['ploeg_id'], $teampoints)) ? $teampoints[$row['ploeg_id']] + $row['points'] : $row['points'];
+            $teampoints[$row['ploeg_id']] = (array_key_exists($row['ploeg_id'], $teampoints)) ? $teampoints[$row['ploeg_id']] + $row['points'] : $row['points'];
             $map[$row['ploeg_id']][$row['date']] = array('date' => $row['date'], 'points' => $teampoints[$row['ploeg_id']]);
         }
         // start-date
         $start = new \DateTime();
-        $start->setDate(date('Y'),1,1)->setDate(0,0,0);
-        // end-date is the end of the year
-        $end = new \DateTime();
-        $end->setDate(date('Y'),12,31)->setTime(23,59,59);
+        $start->setDate(date('Y'), 1, 1)->setTime(0, 0, 0);
+        // end date
+        $stmt = $em->getConnection()->prepare("SELECT DATE_FORMAT(w.datum,'%Y-%m-%d 00:00:00') AS maxdate
+             FROM Wedstrijd w WHERE w.seizoen_id = :seizoen ORDER BY w.datum DESC LIMIT 1");
+        $stmt->execute($params);
+        $maxDate = $stmt->fetchAll();
+        $end = \DateTime::createFromFormat('Y-m-d 00:00:00', !empty($maxDate) ? $maxDate[0]['maxdate'] : date('Y-12-31 00:00:00'));
+        $checkDate = clone $start;
+        $checkDate->modify("+1 month");
+        if ($end < $checkDate) {
+            $end = $checkDate;
+        } else if ($end < new \DateTime()) {
+            $end = new \DateTime();
+            $end->setTime(0, 0, 0);
+        }
         // prepare all months
         $months = array();
         do {
@@ -96,10 +107,9 @@ class TeamController extends \FOS\RestBundle\Controller\FOSRestController
         $ret = array();
         $now = new \DateTime();
         $now->modify('first day of this month');
-
         foreach ($em->getRepository("CyclearGameBundle:Ploeg")->createQueryBuilder('t')->where('t.seizoen = :seizoen')
-            ->setParameter('seizoen', $seizoen)->getQuery()->getResult() as $team) {
-            $mapData = (array_key_exists($team->getId(), $map) ) ? $map[$team->getId()] : array();
+                     ->setParameter('seizoen', $seizoen)->getQuery()->getResult() as $team) {
+            $mapData = (array_key_exists($team->getId(), $map)) ? $map[$team->getId()] : array();
             $points = 0;
             foreach ($months as $month) {
                 $monthFormat = $month->format('Ym');
