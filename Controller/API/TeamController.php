ba4ef4aff413c413c7a4625480ffc8448563426e
Erik Trapman <veggatron@gmail.com>
2014-01-18 19:23:02 +0100

    - different way of creating teamGraph

--- Controller/API/TeamController.php
+++ Controller/API/TeamController.php
@@ -11,6 +11,7 @@
 
 namespace Cyclear\GameBundle\Controller\API;
 
+use Doctrine\ORM\NoResultException;
 use FOS\RestBundle\Controller\Annotations\QueryParam;
 use FOS\RestBundle\Controller\Annotations\RouteResource;
 use JMS\Serializer\SerializationContext;
@@ -61,67 +62,69 @@ class TeamController extends \FOS\RestBundle\Controller\FOSRestController
         return $this->handleView($view);
     }
 
-    public function cgetPointsAction($seizoen)
+    public function getPointsAction($seizoen, $team)
     {
-        /* @type \Doctrine\ORM\EntityManager */
         $em = $this->getDoctrine()->getManager();
         $seizoen = $em->getRepository("CyclearGameBundle:Seizoen")->findOneBySlug($seizoen);
-        $params = array('seizoen' => $seizoen->getId());
-        $stmt = $em->getConnection()->prepare("
-            SELECT SUM(u.ploegPunten) AS points, u.ploeg_id, DATE_FORMAT(w.datum,'%Y%m') AS date
-            FROM Uitslag u 
-                INNER JOIN Wedstrijd w ON u.wedstrijd_id = w.id
-                WHERE w.seizoen_id = :seizoen AND u.ploeg_id IS NOT NULL
-                GROUP BY u.ploeg_id, date
-                ORDER BY u.ploeg_id, date");
-        $stmt->execute($params);
-        $map = array();
-        $teampoints = array();
-        foreach ($stmt->fetchAll() as $row) {
-            $teampoints[$row['ploeg_id']] = (array_key_exists($row['ploeg_id'], $teampoints)) ? $teampoints[$row['ploeg_id']] + $row['points'] : $row['points'];
-            $map[$row['ploeg_id']][$row['date']] = array('date' => $row['date'], 'points' => $teampoints[$row['ploeg_id']]);
+        $ploeg = $em->getRepository("CyclearGameBundle:Ploeg")->find($team);
+
+        $qb = $em->getRepository("CyclearGameBundle:Uitslag")
+            ->createQueryBuilder('u')
+            ->innerJoin('u.wedstrijd', 'w')
+            ->where('u.ploeg = :ploeg')
+            ->andWhere('w.seizoen = :seizoen')
+            ->addSelect('SUM(u.ploegPunten) AS ttl')
+            ->addSelect('DATE_FORMAT(w.datum, \'%Y%m%d\') AS groupdate')
+            ->addSelect('w')
+            ->groupBy('groupdate')->setParameters(array('seizoen' => $seizoen, 'ploeg' => $ploeg));
+
+        //echo $qb->getDQL();die;
+        $res = array();
+        foreach ($qb->getQuery()->getArrayResult() as $row) {
+            $res[$row['groupdate']] = $row['ttl'];
         }
-        // start-date
-        $start = new \DateTime();
-        $start->setDate(date('Y'), 1, 1)->setTime(0, 0, 0);
-        // end date
-        $stmt = $em->getConnection()->prepare("SELECT DATE_FORMAT(w.datum,'%Y-%m-%d 00:00:00') AS maxdate
-             FROM Wedstrijd w WHERE w.seizoen_id = :seizoen ORDER BY w.datum DESC LIMIT 1");
-        $stmt->execute($params);
-        $maxDate = $stmt->fetchAll();
-        $end = \DateTime::createFromFormat('Y-m-d 00:00:00', !empty($maxDate) ? $maxDate[0]['maxdate'] : date('Y-12-31 00:00:00'));
-        $checkDate = clone $start;
-        $checkDate->modify("+1 month");
-        if ($end < $checkDate) {
-            $end = $checkDate;
-        } else if ($end < new \DateTime()) {
-            $end = new \DateTime();
-            $end->setTime(0, 0, 0);
+        try {
+            $start = $em
+                ->createQuery(sprintf("SELECT MIN(w.datum) AS maxdate FROM CyclearGameBundle:Wedstrijd w WHERE w.seizoen = %d", $seizoen->getId()))
+                ->getSingleScalarResult();
+            $start = new \DateTime($start);
+            $start->modify("-1 day");
+        } catch (NoResultException $e) {
+            $start = new \DateTime(date('Y'), 1, 1);
+            $start->setTime(0, 0, 0);
         }
-        // prepare all months
-        $months = array();
-        do {
-            $months[] = clone $start;
-            $start->modify("+1 month");
-        } while ($start < $end);
+
+        try {
+            $possibleEndDate = $em
+                ->createQuery(sprintf("SELECT MAX(w.datum) AS maxdate FROM CyclearGameBundle:Wedstrijd w WHERE w.seizoen = %d", $seizoen->getId()))
+                ->getSingleScalarResult();
+            $possibleEndDate = new \DateTime($possibleEndDate);
+            $possibleEndDate->modify("+1 week");
+        } catch (NoResultException $e) {
+            $possibleEndDate = clone $start;
+        }
+        $end = clone $start;
+        $end->modify("+1 week");
+        if ($possibleEndDate > $end) {
+            $end = $possibleEndDate;
+        }
+
         $ret = array();
-        $now = new \DateTime();
-        $now->modify('first day of this month');
-        foreach ($em->getRepository("CyclearGameBundle:Ploeg")->createQueryBuilder('t')->where('t.seizoen = :seizoen')
-                     ->setParameter('seizoen', $seizoen)->getQuery()->getResult() as $team) {
-            $mapData = (array_key_exists($team->getId(), $map)) ? $map[$team->getId()] : array();
-            $points = 0;
-            foreach ($months as $month) {
-                $monthFormat = $month->format('Ym');
-                if (array_key_exists($monthFormat, $mapData) && $month <= $now) {
-                    $points = $mapData[$monthFormat]['points'];
-                } elseif ($month > $now) {
-                    $points = null;
-                }
-                $ret[$team->getId()][$monthFormat] = array('date' => $monthFormat, 'points' => $points);
-            }
+        while ($start < $end) {
+            $key = $start->format('Ymd');
+            $ret[$key] = 0;
+            $start->modify("+1 day");
         }
-        $view = $this->view($ret, 200)->setSerializationContext(SerializationContext::create()->setSerializeNull(true));
+
+        $ttl = 0;
+        foreach ($ret as $key => $value) {
+            if (array_key_exists($key, $res)) {
+                $ttl += $res[$key];
+            }
+            $ret[$key] = $ttl;
+        }
+
+        $view = $this->view($ret, 200);
         return $this->handleView($view);
     }
 }
--- Resources/public/js/Directive/teamGraph.js
+++ Resources/public/js/Directive/teamGraph.js
@@ -13,7 +13,6 @@ angular.module('Cyclear', ['d3']).directive('teamGraph', ['d3Service', function 
             $scope.data = [];
             $scope.selected = [];
             $scope.colors = [];
-            $scope.finishedLoading = false;
 
             $scope.getTeams = function () {
                 // TODO set up FOS JsRouting !!! Really !
@@ -23,8 +22,10 @@ angular.module('Cyclear', ['d3']).directive('teamGraph', ['d3Service', function 
                         $scope.teams[i] = data[i];
                         $scope.teamsById[data[i].id] = data[i];
                         $scope.colors[data[i].id] = $scope.color(data[i].id);
+                        if (i < 3) {
+                            $scope.setSelect(data[i].id);
+                        }
                     }
-                    $scope.getPoints();
                 });
             }
 
@@ -32,13 +33,19 @@ angular.module('Cyclear', ['d3']).directive('teamGraph', ['d3Service', function 
                 var index = $scope.selected.indexOf(id);
                 if (index > -1) {
                     $scope.selected.splice(index, 1);
+                    $scope.clearTeam(id);
                 } else {
                     $scope.selected.push(id);
+                    // TODO get a promise from getPoints and call render(id) here
+                    $scope.getPoints(id);
                 }
             }
 
             d3Service.d3().then(function (d3) {
                 $scope.getTeams();
+
+                var prerendered = false;
+                var svg = null;
 
                 var margin = {top: 20, right: 80, bottom: 30, left: 50},
                     width = 800 - margin.left - margin.right,
@@ -48,13 +55,13 @@ angular.module('Cyclear', ['d3']).directive('teamGraph', ['d3Service', function 
                 var y = d3.scale.linear().range([height, 0]);
 
                 // we get dates in a 201301 -format
-                var parseDate = d3.time.format("%Y%m").parse;
+                var parseDate = d3.time.format("%Y%m%d").parse;
 
                 $scope.color = d3.scale.category20();
 
                 var xAxis = d3.svg.axis()
                     .scale(x)
-                    .orient("bottom").tickFormat(d3.time.format("%b"));
+                    .orient("bottom").tickFormat(d3.time.format("%d-%m"));
 
                 var yAxis = d3.svg.axis()
                     .scale(y)
@@ -82,28 +89,24 @@ angular.module('Cyclear', ['d3']).directive('teamGraph', ['d3Service', function 
                 }
 
                 var prerender = function () {
-                    var svg = svg2();
+                    svg = svg2();
                     svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + height + ")").call(xAxis);
                     svg.append("g").attr("class", "y axis").call(yAxis);
-                    return svg;
+                    prerendered = true;
                 }
 
-                $scope.getPoints = function () {
-                    $http.get("/api/seasons/" + $scope.seizoen + "/teams/points.json").success(function (data) {
+                $scope.getPoints = function (teamId) {
+                    if ($scope.data[teamId]) {
+                        $scope.render(teamId);
+                        return;
+                    }
+
+                    $http.get("/api/seasons/" + $scope.seizoen + "/teams/" + teamId + "/points.json").success(function (data) {
+                        var teamData = [];
                         for (i in data) {
-                            var teamData = [];
-                            for (j in data[i]) {
-                                teamData.push({'date': parseDate(data[i][j].date), 'points': data[i][j].points});
-                            }
-                            $scope.data[i] = teamData;
+                            teamData.push({'date': parseDate(i), 'points': data[i]});
                         }
-                        // teams are ordered by points already
-                        for (i in $scope.teams) {
-                            if (i > 9 || 0 == $scope.teams[i].punten) {
-                                break;
-                            }
-                            $scope.selected.push($scope.teams[i].id);
-                        }
+                        $scope.data[teamId] = teamData;
                         for (i in $scope.data) {
                             x.domain(d3.extent($scope.data[i], function (d) {
                                 return d.date;
@@ -114,31 +117,39 @@ angular.module('Cyclear', ['d3']).directive('teamGraph', ['d3Service', function 
                         y.domain([0, d3.max($scope.teams, function (team) {
                             return parseInt(team.punten, 10);
                         }) + 50]);
-                        $scope.finishedLoading = true;
-                        $scope.render();
+
+                        // TODO return a promise and render() somewhere else
+                        $scope.render(teamId);
+
                     });
+
                 }
 
-                $scope.$watch('selected', function (newvalue, oldvale) {
-                    if ($scope.finishedLoading) {
-                        $scope.render();
-                    }
-                }, true);
+                $scope.clearTeam = function (teamId) {
+                    d3.select("#line_" + teamId).remove();
+                    d3.select("#text_" + teamId).remove();
+                }
 
-                $scope.render = function () {
-                    d3.select("svg").remove();
-                    var svg = prerender();
-                    for (i in $scope.selected) {
-                        var d = $scope.data[$scope.selected[i]];
-                        var teamId = $scope.selected[i];
-                        svg
-                            .append("path").attr("class", "line").attr('id', 'line_' + i)
-                            .style("stroke", $scope.colors[teamId])
-                            .attr("d", line(d))
-                        ;
-                        svg.append("text").text($scope.teamsById[teamId].afkorting).attr("x", 3).attr("dy", ".35em")
-                            .attr("transform", "translate(" + width + "," + y(parseInt($scope.teamsById[teamId].punten, 10)) + ")")
+                $scope.render = function (teamId) {
+                    //d3.select("svg").remove(); // we remove the complete svg. so we have to re-render all.
+                    if(!prerendered){
+                        prerender();
                     }
+                    //for (i in $scope.selected) {
+                    var d = $scope.data[teamId];
+                    //var teamId = $scope.selected[i];
+                    svg
+                        .append("path").attr("class", "line").attr('id', 'line_' + teamId)
+                        .style("stroke", $scope.colors[teamId])
+                        .attr("d", line(d))
+                    ;
+                    svg.append("text").text($scope.teamsById[teamId].afkorting).
+                        attr("x", 3).
+                        attr("dy", ".35em")
+                        .attr("transform", "translate(" + width + "," + y(parseInt($scope.teamsById[teamId].punten, 10)) + ")")
+                        .attr("id", "text_" + teamId)
+                    //}
+
                 }
             });
         }
