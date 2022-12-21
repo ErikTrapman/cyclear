3c6918f8fba6970dc7987215e067de8612cde6c6
Erik Trapman <erikt@fabrique.nl>
2022-12-21 20:13:09 +0100

    Upd deploys

--- .surf/prod.php
+++ .surf/prod.php
@@ -2,6 +2,9 @@
 
 namespace Cyclear;
 
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+use Symfony\Component\Dotenv\Dotenv;
 use TYPO3\Surf\Application\BaseApplication;
 use TYPO3\Surf\Domain\Model\Deployment;
 use TYPO3\Surf\Domain\Model\Node;
@@ -9,12 +12,13 @@ use TYPO3\Surf\Task\Php\WebOpcacheResetCreateScriptTask;
 use TYPO3\Surf\Task\Php\WebOpcacheResetExecuteTask;
 use TYPO3\Surf\Task\ShellTask;
 
-//$node = new Node('cloud-04.veggatron.nl'); ENV_VAR from cli
-//$node = new Node('test.cyclear.nl'); ENV_VAR from cli
-$node = new Node('cyclear5.cyclear.nl');
+$env = new Dotenv();
+$env->bootEnv(__DIR__.'/../.env');
+
+$node = new Node($_ENV['SURF_PROD_HOST']); // ENV_VAR from cli
 $node->setHostname($node->getName());
-$node->setOption('username', 'flat'); // ENV_VAR from cli
-$node->setDeploymentPath('/home/flat/cyclear5'); // ENV_VAR from cli
+$node->setOption('username', $_ENV['SURF_PROD_USERNAME']); // ENV_VAR from cli
+$node->setDeploymentPath($_ENV['SURF_PROD_PATH']); // ENV_VAR from cli
 $node->setOption('phpBinaryPathAndFilename', '/usr/bin/php');
 
 /** @var Deployment $deployment */
@@ -23,10 +27,9 @@ $deployment->setWorkspacesBasePath(__DIR__ . '/builds');
 $application = new BaseApplication('cyclear');
 $application->addNode($node);
 $application->setOption('repositoryUrl', 'git@github.com:ErikTrapman/cyclear.git');
-//$application->setOption('branch', 'release/5.x');
-$application->setOption('branch', 'feature/deploy');
+$application->setOption('branch', 'release/5.x');
 $application->setOption('composerCommandPath', 'composer');
-$application->setOption('baseUrl', 'http://cyclear5.cyclear.nl');
+$application->setOption('baseUrl', $_ENV['SURF_PROD_BASEURL']);
 $deployment->addApplication($application);
 
 $deployment->onInitialize(
@@ -35,13 +38,21 @@ $deployment->onInitialize(
 
         $workflow->defineTask('CopyEnvFileTask', ShellTask::class, [
             'command' => [
-                'cp {sharedPath}/.env.local.php {releasePath}/.env.local.php',
+                'cp {sharedPath}/.env.local.dist {releasePath}/.env.local.php',
             ],
+        ]);
+
+        $workflow->defineTask('RemoteTasks', ShellTask::class, [
+           'command' => [
+               'cd {releasePath} && bin/console cache:clear',
+               'cd {releasePath} && bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration',
+           ],
         ]);
 
         $workflow
             ->beforeStage('transfer', WebOpcacheResetCreateScriptTask::class, $application)
             ->afterStage('transfer', 'CopyEnvFileTask', $application)
+            ->forStage('migrate', 'RemoteTasks')
             ->afterStage('switch', WebOpcacheResetExecuteTask::class, $application);
     }
 );
--- public/index.php
+++ public/index.php
@@ -1,8 +1,5 @@
 <?php
 
-echo "another one bites the dust 21-12-22 19:42";
-die;
-
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
