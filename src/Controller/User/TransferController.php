04a538fec7a64e567a302c0418b02b90505e25c6
Erik Trapman <veggatron@gmail.com>
2023-07-07 15:10:25 +0200

    Mainly fix UserTransfers

--- src/Controller/User/TransferController.php
+++ src/Controller/User/TransferController.php
@@ -4,7 +4,6 @@ namespace App\Controller\User;
 
 use App\Entity\Ploeg;
 use App\Entity\Renner;
-use App\Entity\Seizoen;
 use App\EntityManager\TransferManager;
 use App\EntityManager\UserManager;
 use App\Form\Entity\UserTransfer;
@@ -14,7 +13,9 @@ use App\Repository\RennerRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -30,25 +31,24 @@ class TransferController extends AbstractController
     ) {
     }
 
-    #[Route(path: '/user/{seizoen}/transfer/ploeg/{id}/renner/{renner}', name: 'user_transfer')]
+    #[Route(path: '/user/transfer/ploeg/{id}/renner/{renner}', name: 'user_transfer')]
     public function indexAction(
         UserManager $userManager,
         TransferManager $transferManager,
         Request $request,
-        Seizoen $seizoen,
         Ploeg $ploeg,
-        #[MapEntity(mapping: ['renner' => 'slug'])] Renner $renner): \Symfony\Component\HttpFoundation\Response
+        #[MapEntity(mapping: ['renner' => 'slug'])] Renner $renner): Response|RedirectResponse
     {
         if (!$userManager->isOwner($this->getUser(), $ploeg)) {
             throw new AccessDeniedHttpException('Dit is niet jouw ploeg');
         }
         $transferUser = new UserTransfer();
         $transferUser->setPloeg($ploeg);
-        $transferUser->setSeizoen($seizoen);
+        $transferUser->setSeizoen($ploeg->getSeizoen());
         $transferUser->setDatum(new \DateTime());
 
         $options = [];
-        $rennerPloeg = $this->rennerRepository->getPloeg($renner, $seizoen);
+        $rennerPloeg = $this->rennerRepository->getPloeg($renner, $ploeg->getSeizoen());
         if ($rennerPloeg !== $ploeg) {
             if (null !== $rennerPloeg) {
                 throw new AccessDeniedHttpException('Renner is niet in je ploeg');
@@ -67,22 +67,22 @@ class TransferController extends AbstractController
             $form->handleRequest($request);
             if ($form->isValid()) {
                 if ($rennerPloeg !== $ploeg) {
-                    $transferManager->doUserTransfer($ploeg, $form->get('renner_uit')->getData(), $renner, $seizoen, $form->get('userComment')->getData());
+                    $transferManager->doUserTransfer($ploeg, $form->get('renner_uit')->getData(), $renner, $ploeg->getSeizoen(), $form->get('userComment')->getData());
                 } else {
-                    $transferManager->doUserTransfer($ploeg, $renner, $form->get('renner_in')->getData(), $seizoen, $form->get('userComment')->getData());
+                    $transferManager->doUserTransfer($ploeg, $renner, $form->get('renner_in')->getData(), $ploeg->getSeizoen(), $form->get('userComment')->getData());
                 }
                 $this->doctrine->getManager()->flush();
-                return $this->render('user/transfer/index.html.twig');
+                return new RedirectResponse($this->generateUrl('ploeg_show', ['seizoen' => $ploeg->getSeizoen()->getSlug(), 'id' => $ploeg->getId()]));
             }
         }
         $transferInfo = $transferManager->getTtlTransfersDoneByPloeg($ploeg);
-        $ttlTransfersAtm = $transferManager->getTtlTransfersAtm($seizoen);
+        $ttlTransfersAtm = $transferManager->getTtlTransfersAtm($ploeg->getSeizoen());
         return
             $this->render('user/transfer/index.html.twig', [
                 'ploeg' => $ploeg,
                 'renner' => $renner,
                 'form' => $form->createView(),
-                'seizoen' => $seizoen,
+                'seizoen' => $ploeg->getSeizoen(),
                 'transferInfo' => [
                     'count' => $transferInfo,
                     'left' => $ttlTransfersAtm - $transferInfo,
